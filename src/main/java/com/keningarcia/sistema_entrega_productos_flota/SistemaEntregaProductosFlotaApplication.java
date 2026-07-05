03738564909ddcf4d884c4504284fea342bfbaca
keningarcia <kenin.garciacampos@gmail.com>
2026-07-04 21:17:26 -0500

    Creación del proyecto

---
+++ src/main/java/com/keningarcia/sistema_entrega_productos_flota/SistemaEntregaProductosFlotaApplication.java
@@ -0,0 +1,13 @@
+package com.keningarcia.sistema_entrega_productos_flota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEntregaProductosFlotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEntregaProductosFlotaApplication.class, args);
+	}
+
+}
